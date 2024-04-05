bf2c286f1143afdb94bb2b003604f099377021db
dohalou99 <dohaelouakili99@gmai.com>
2024-04-05 01:12:32 +0000

    dodo

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python3
-
 """
 UTF-8 Validation
 """
-def validUTF8(data):
+
+
+def validUTF8(data) -> bool:
+    """
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
+    """
     num_bytes = 0
     for byte in data:
-        # Check if current byte is a start byte
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # Check if current byte is a continuation byte
-            if (byte >> 6) != 0b10:
+            if byte >> 6 != 0b10:
                 return False
-            num_bytes -= 1
+        num_bytes -= 1
     return num_bytes == 0
